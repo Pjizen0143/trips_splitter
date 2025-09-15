299e9b96398320897dd1904085dae619b21babe0
Pjizen0143 <parabolic0143@gmail.com>
2025-09-15 19:28:55 +0700

    ✨ add expire time
    delete data that hasn't been updated for 10 days

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from typing import List
 from dotenv import load_dotenv
 import os
-
-from fastapi import FastAPI, HTTPException
-from pymongo import MongoClient
-from pydantic import BaseModel
-from bson.objectid import ObjectId
-
 import random
 import string
+
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient, ASCENDING
+from pydantic import BaseModel
+from datetime import datetime, timezone
 
 
 def random_key() -> str:
@@ -23,6 +22,12 @@ client = MongoClient(os.getenv("MONGO_URI"))
 db = client[os.getenv("DB_CLIENT")]
 collection = db[os.getenv("DB_COLLECTION")]
 
+
+collection.create_index(
+    [("last_updated", ASCENDING)],
+    expireAfterSeconds=10 * 24 * 60 * 60  # 10 days
+)
+
 class CreateTrip(BaseModel):
     trip_name: str
     members: List[str]
@@ -31,6 +36,8 @@ class CreateTrip(BaseModel):
 
 class Trip(CreateTrip):
     trip_key: str
+    last_updated: datetime
+
 
 @app.post("/trips")
 async def create_trip(trip: CreateTrip):
@@ -38,15 +45,18 @@ async def create_trip(trip: CreateTrip):
         raise HTTPException(status_code=400, detail="Expenses and members don't have the same length")
 
     key = random_key()
-    while collection.find_one({"trip_key": key} is not None):
+    while collection.find_one({"trip_key": key}) is not None:
         key = random_key()
 
-    trip_with_key = Trip(**trip.model_dump(), trip_key=key)
+    now = datetime.now(timezone.utc)
 
+    trip_with_key = Trip(**trip.model_dump(), trip_key=key, last_updated=now)
     result = collection.insert_one(trip_with_key.model_dump())
+
     return {
         "trip_id": str(result.inserted_id),
         "trip_key": trip_with_key.trip_key,
         "trip_name": trip_with_key.trip_name,
         "members": trip_with_key.members,
+        "last_updated": now,
     }
