6693a13e1aacaea517590125d920f127a72a8ee1
Pjizen0143 <parabolic0143@gmail.com>
2025-10-13 21:09:02 +0700

    🎨 edit splitter func return

--- splitter.py
+++ splitter.py
@@ -1,6 +1,5 @@
 from typing import List
 from models import ResultItem, Result, Response
-from fastapi import HTTPException
 
 def more_than_avg(expenses: List[float], avg: float) -> int:
     count = 0
@@ -27,17 +26,23 @@ def less_intermediary(expenses: List[float], avg: float):
 
 def splitter(expenses: List[float], members: List[str]):
     if len(expenses) != len(members):
-        raise HTTPException(status_code=400, detail="Expenses and members length don't match")
+        return Response(status="Error", message="Expenses do not have the same amount of members")
 
     avg = sum(expenses) / len(expenses)
     sum_of_more_than = more_than_avg(expenses, avg)
 
+    _Result = []
 
     if sum_of_more_than == 1:
         inter_member = intermediary(expenses, avg)
-        for i, expense in enumerate(expenses):
+        for i, expense in enumerate(expenses): # enumerate is return index and item in list
             if expense < avg:
-                print(f"{members[i]} -> {members[inter_member]}: {avg - expense}")
+                _resultItem = ResultItem(
+                    from_member=str(members[i]),
+                    to=str(members[inter_member]),
+                    amount=avg - expense
+                )
+                _Result.append(_resultItem)
 
     elif sum_of_more_than > 1:
         sum_of_less_than = len(members) - sum_of_more_than
@@ -45,18 +50,31 @@ def splitter(expenses: List[float], members: List[str]):
             inter_member = less_intermediary(expenses, avg)
             for i, expense in enumerate(expenses):
                 if expense > avg:
-                    print(f"{members[inter_member]} -> {members[i]}: {expense - avg}")
+                    _resultItem = ResultItem(
+                        from_member=str(members[inter_member]),
+                        to=str(members[i]),
+                        amount=expense - avg
+                    )
+                    _Result.append(_resultItem)
 
         elif sum_of_less_than > 1:
             inter_member = 0
             for i, expense in enumerate(expenses[1:], 1):
                 if expense < avg:
-                    print(f"{members[i]} -> {members[inter_member]}: {avg - expense}")
+                    if expense > avg:
+                        _resultItem = ResultItem(
+                            from_member=str(members[i]),
+                            to=str(members[inter_member]),
+                            amount=avg - expense
+                        )
+                        _Result.append(_resultItem)
             for i, expense in enumerate(expenses[1:], 1):
                 if expense > avg:
-                    print(f"{members[inter_member]} -> {members[i]}: {expense - avg}")
-
-if __name__ == "__main__":
-    input_members = ["Phone", "milk", "chopoo", "bomb"]
-    input_expenses = [0, 500, 500, 0]
-    splitter(input_expenses, input_members)
+                    if expense > avg:
+                        _resultItem = ResultItem(
+                            from_member=str(members[inter_member]),
+                            to=str(members[i]),
+                            amount=expense - avg
+                        )
+                        _Result.append(_resultItem)
+    return Response(status="Complete", message=Result(settlements=_Result))
