39fd9ed36786aaa965bcc193ca21b3ff8de43804
Pjizen0143 <parabolic0143@gmail.com>
2025-10-13 18:14:02 +0700

    ✨ Add new models for splitter func

--- models.py
+++ models.py
@@ -1,15 +1,31 @@
-from typing import List
+from typing import List, Any
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
 
 class CreateTrip(BaseModel):
     trip_name: str
     members: List[str]
-    expenses: List[str]
+    expenses: List[float]
 
 
 class Trip(CreateTrip):
-    trip_key: str
+    trip_code: str
     last_updated: datetime
 
+
+class ResultItem(BaseModel):
+    from_member: str = Field(..., alias="from")
+    to: str
+    amount: float
+
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class Result(BaseModel):
+    settlements: List[ResultItem]
+
+
+class Response(BaseModel):
+    status: str
+    message: Any
